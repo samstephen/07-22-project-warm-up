756b696aa883ec5c523412cbaf08969791805a02
Sam Stephen <samueljstephen@gmail.com>
2015-07-22 13:25:25 -0500

    added crud to user_controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,26 @@
+class UserController < ApplicationController
+  def index
+    @user = User.all
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def user_params
+  end
+end
