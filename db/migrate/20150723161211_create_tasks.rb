a7f9045e1050cdaa47e463aa67db126678dae5f8
Sam Stephen <samueljstephen@gmail.com>
2015-07-23 11:22:59 -0500

    corrected issues

--- db/migrate/20150723161211_create_tasks.rb
+++ db/migrate/20150723161211_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-      t.string :[title
+      t.string :title
       t.text :description
-      t.integer] :user_id
+      t.integer :user_id
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722175601) do
+ActiveRecord::Schema.define(version: 20150723161211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text     "name"
